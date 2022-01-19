f4adc9eef411c447d7fcd5f58ee547490b446822
Moaad Fattali <moaad.fattali@ovrsea.com>
2022-01-19 20:54:42 +0100

    feat: bump node version for proper logging

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import fastify from "fastify";
-import { logErrorWithCause } from "./printErrorWithCause";
 import { getShipmentById } from "./repository";
 
-const fetchShipmentById = async (shipmentId: string) => {
+const fetchShipmentById = (shipmentId: string) => {
   try {
     return getShipmentById(shipmentId);
   } catch (error) {
@@ -14,27 +12,40 @@ const fetchShipmentById = async (shipmentId: string) => {
   }
 };
 
-fetchShipmentById("0").then(console.log).catch(logErrorWithCause);
+//fetchShipmentById("0");
 
 // Error: Failed to get shipment with id 0
-// at /Users/moaadf/node-error-cause/build/index.js:47:19
-// at step (/Users/moaadf/node-error-cause/build/index.js:33:23)
-// at Object.next (/Users/moaadf/node-error-cause/build/index.js:14:53)
-// at /Users/moaadf/node-error-cause/build/index.js:8:71
-// at new Promise (<anonymous>)
-// at __awaiter (/Users/moaadf/node-error-cause/build/index.js:4:12)
-// at fetchShipmentById (/Users/moaadf/node-error-cause/build/index.js:41:56)
-// at Object.<anonymous> (/Users/moaadf/node-error-cause/build/index.js:54:1)
-// at Module._compile (node:internal/modules/cjs/loader:1101:14)
-// at Object.Module._extensions..js (node:internal/modules/cjs/loader:1153:10)
-// Cause: Error: Shipment with id 0 does not exist
-// at getShipmentById (/Users/moaadf/node-error-cause/build/repository.js:33:15)
-// at /Users/moaadf/node-error-cause/build/index.js:44:68
-// at step (/Users/moaadf/node-error-cause/build/index.js:33:23)
-// at Object.next (/Users/moaadf/node-error-cause/build/index.js:14:53)
-// at /Users/moaadf/node-error-cause/build/index.js:8:71
-// at new Promise (<anonymous>)
-// at __awaiter (/Users/moaadf/node-error-cause/build/index.js:4:12)
-// at fetchShipmentById (/Users/moaadf/node-error-cause/build/index.js:41:56)
-// at Object.<anonymous> (/Users/moaadf/node-error-cause/build/index.js:54:1)
-// at Module._compile (node:internal/modules/cjs/loader:1101:14)
+//     at fetchShipmentById (/Users/moaadf/node-error-cause/build/index.js:9:15)
+//     at Object.<anonymous> (/Users/moaadf/node-error-cause/build/index.js:14:1)
+//     ... 5 lines matching cause stack trace ...
+//     at node:internal/main/run_main_module:17:47 {
+//   [cause]: Error: Shipment with id 0 does not exist
+//       at getShipmentById (/Users/moaadf/node-error-cause/build/repository.js:33:15)
+//       at fetchShipmentById (/Users/moaadf/node-error-cause/build/index.js:6:49)
+//       at Object.<anonymous> (/Users/moaadf/node-error-cause/build/index.js:14:1)
+//       at Module._compile (node:internal/modules/cjs/loader:1097:14)
+//       at Object.Module._extensions..js (node:internal/modules/cjs/loader:1149:10)
+//       at Module.load (node:internal/modules/cjs/loader:975:32)
+//       at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+//       at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:77:12)
+//       at node:internal/main/run_main_module:17:47
+// }
+
+//fetchShipmentById("2");
+
+// Error: Failed to get shipment with id 2
+//     at fetchShipmentById (/Users/moaadf/node-error-cause/build/index.js:9:15)
+//     at Object.<anonymous> (/Users/moaadf/node-error-cause/build/index.js:31:1)
+//     ... 5 lines matching cause stack trace ...
+//     at node:internal/main/run_main_module:17:47 {
+//   [cause]: Error: Cannot view shipment with id 2: shipment has been cancelled
+//       at getShipmentById (/Users/moaadf/node-error-cause/build/repository.js:36:15)
+//       at fetchShipmentById (/Users/moaadf/node-error-cause/build/index.js:6:49)
+//       at Object.<anonymous> (/Users/moaadf/node-error-cause/build/index.js:31:1)
+//       at Module._compile (node:internal/modules/cjs/loader:1097:14)
+//       at Object.Module._extensions..js (node:internal/modules/cjs/loader:1149:10)
+//       at Module.load (node:internal/modules/cjs/loader:975:32)
+//       at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+//       at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:77:12)
+//       at node:internal/main/run_main_module:17:47
+// }
